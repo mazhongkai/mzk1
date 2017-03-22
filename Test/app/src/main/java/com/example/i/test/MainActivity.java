1f0b752de693de56bb3bde845b0dc4d5bf0fd1f5
unknown <>
2017-03-22 18:16:32 +0800

    1

--- Test/app/src/main/java/com/example/i/test/MainActivity.java
+++ Test/app/src/main/java/com/example/i/test/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.i.test;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EMClient.getInstance().login("456","654",new EMCallBack() {//回调
+            @Override
+            public void onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                Log.d("main", "登录聊天服务器成功！");
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.d("main", "登录聊天服务器失败！");
+            }
+        });
     }
 }
